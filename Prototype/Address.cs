f405071c873775a1cd5ef9a8f9984fe540fb07a9
jobin jose samuel <jobin.samuel@outlook.com>
2024-10-12 20:29:01 +0530

    prototype

--- Prototype/Address.cs
+++ Prototype/Address.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Prototype
 {
-    internal class Address
+    public class Address
     {
         public string Address1 { get; set; }
         public string Address2 { get; set; }
--- Prototype/User.cs
+++ Prototype/User.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Prototype
 {
-    internal abstract class User : ICloneable
+    public abstract class User : ICloneable
     {
         public string FirstName {  get; set; }
         public string LastName { get; set; }
         public string UserName {  get; set; }
         public string Password { get; set; }
-        public Address Address { get; set; }
+        public Address HomeAddress { get; set; }
         public Address BusinessAddress { get; set; }
         
         public abstract object Clone();
