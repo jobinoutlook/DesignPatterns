c17f29f27ae3760d37a8dcbd9cf97355d81585ff
jobin jose samuel <jobin.samuel@outlook.com>
2024-10-12 21:41:44 +0530

    singleton

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private Singleton _singleton;
+
+        private Singleton() { }
+
+        public Singleton GetSingleton()
+        {
+            if (_singleton == null)
+            {
+                _singleton = new Singleton();   
+            }
+            
+                return _singleton;
+            
+        }
+
+    }
+}
